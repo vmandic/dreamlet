0154343568581ab452a182cc1bb642951400c41b
vmandic-five <vedran.mandic@fiveminutes.eu>
2016-10-26 11:14:46 +0200

    initial scrape testing

--- scraper/index.js
+++ scraper/index.js
@@ -1,39 +1,44 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var app = express();
+require("./common.js");
+
+var express = require('express'),
+    fs = require('fs'),
+    atob = require("atob"),
+    request = require('request'),
+    limit = require("simple-rate-limiter"),
+    cheerio = require('cheerio'),
+    app = express();
+
+var limitedRequest = limit(request).to(2).per(5000);
+
+var scrape1 = function () {
+    var baseUrl = "{0}/dreamdictionary/{1}_all.html".format(atob("aHR0cDovL3d3dy5kcmVhbW1vb2RzLmNvbQ=="));
+
+    var requests = [];
+
+    ["abcdefghijklmnopqrstuvwxyz"].forEach(function (letter, index) {
+        requests.push(function () {
+            var url = baseUrl.format("", letter);
+            limitedRequest(url, processScrape1);
+        });
+    });
+
+    // TEST: explicitly trigger the first one, letter a
+    requests[0]();
+};
+
+var processScrape1 = function (err, resp, html) {
+    if (!err) {
+        var $ = cheerio.load(html);
+
+    } else {
+        console.error("Scrape1 ERR: ", err);
+    }
+};
 
 app.get('/scrape', function (req, res) {
-    url = 'http://www.imdb.com/title/tt1229340/';
-
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
-
-    request(url, function (error, response, html) {
-
-        // First we'll check to make sure no errors occurred when making the request
-
-        if (!error) {
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-            var $ = cheerio.load(html);
-
-            // Finally, we'll define the variables we're going to capture
-
-            var title, release, rating;
-            var json = {
-                title: "",
-                release: "",
-                rating: ""
-            };
-        }
-    })
+    scrape1();
 });
 
 app.listen('8081');
-
 console.log('Magic happens on port 8081');
-
 exports = module.exports = app;
