1a67a9beda95b86914b313ee31e03c8a401238d6
Vedran Mandić <vedran.mandic@fiveminutes.eu>
2016-10-28 16:58:41 +0200

    json response, async added

--- scraper/scrapers/scrape1.js
+++ scraper/scrapers/scrape1.js
@@ -2,46 +2,41 @@ var fs = require('fs'),
     atob = require("atob"),
     request = require('request'),
     limit = require("simple-rate-limiter"),
-    cheerio = require('cheerio');
-
-var limitedRequest = limit(request).to(3).per(2500);
+    async = require("async"),
+    cheerio = require('cheerio'),
+    limitedRequest = limit(request).to(3).per(2500);
 
 var scrape1 = function (res) {
-    var baseUrl = "{0}/dreamdictionary/{1}_all.htm".format(atob("aHR0cDovL3d3dy5kcmVhbW1vb2RzLmNvbQ=="));
+    var baseUrl = "{0}/dreamdictionary/{1}_all.htm".format(atob("aHR0cDovL3d3dy5kcmVhbW1vb2RzLmNvbQ==")),
+        requests = [];
 
-    var requests = [];
-
+    // prepare request objects
     "abcdefghijklmnopqrstuvwxyz".split("").forEach(function (letter, index) {
-        requests.push(function (res) {
+        requests.push(function (callback) {
             var url = baseUrl.format("", letter);
-            limitedRequest(url, _processScrape1.bind(this, baseUrl, letter, res));
+            limitedRequest(url, _processScrape1.bind(this, baseUrl, letter, callback));
         });
     });
 
-    requests[0](res);
-
-    //initiate all requests
-    // requests.forEach(function (req, index) {
-    //     req();
-    //     console.log("Launching req id: " + (index + 1));
-    // });
-
-    // how to scrape scrape1 into array of terms and explanations
-
+    // initiate all requests
+    async.parallel(requests, function(error, results){
+        res.setHeader('Content-Type', 'application/json');
+        res.json(results);
+    });
 };
 
-var _processScrape1 = function (baseUrl, letter, res, err, resp, html) {
+var _processScrape1 = function (baseUrl, letter, callback, err, resp, html) {
     if (!err) {
-        var fileName = "tmp/dl/SITE-{0}-LETTER-{1}.html".format(baseUrl.split("//")[1].split("/")[0], letter);
-
-        //_saveToFileSystem(fileName, html);
-        _saveToJson(cheerio.load(html), res);
+        //_saveToFileSystem(baseUrl, letter, html);
+        _saveToJson(cheerio.load(html), callback);
     } else {
         console.error("Scrape1 ERR: ", err);
     }
 };
 
-var _saveToFileSystem = function (fileName, html) {
+var _saveToFileSystem = function (baseUrl, letter, html) {
+    var fileName = "tmp/dl/SITE-{0}-LETTER-{1}.html".format(baseUrl.split("//")[1].split("/")[0], letter);
+
     fs.writeFile(fileName, html, function (fserr) {
         if (fserr)
             console.error("FS ERROR: ", fserr);
@@ -50,18 +45,21 @@ var _saveToFileSystem = function (fileName, html) {
     });
 };
 
-var _saveToJson = function ($, res) {
-    var terms = [], data = [];
-    var currentTerm = null;
+var _saveToJson = function ($, callback) {
+    var terms = [], 
+        data = [],
+        currentTerm = null;
 
+    // filter out data for postprocessing
     data = $("table[width=950] td[width=750]:first-child > p").map(function (index, el) {
         if ($(el).html().indexOf("<b>") > -1 
          || $(el).html().indexOf("<strong>") > -1)
-            return "TERM - " + $(el).text();
+            return "TERM - " + _sanitize($(el).text());
 
-        return $(el).text().replaceAll("\r\n", " ").replaceAll("\r", " ").replaceAll("\n", " ").trim().replaceAll("  ", " ").replaceAll("   ", " ").replace(" TOP", "").trimRight();
+        return _sanitize($(el).text());
     });
 
+    // build term objects
     data.toArray().forEach(function (e, i) {
         if (e.indexOf("TERM - ") > -1) {
             var termName = e.split("TERM - ")[1];
@@ -75,10 +73,12 @@ var _saveToJson = function ($, res) {
             currentTerm && e.length > 0 && currentTerm.explanations.push(e);
         }
     });
+                                                        
+    callback(null, terms.filter(function(t) { return t.explanations.some(function(_t){ return _t.indexOf("function") == -1 }); }));
+};
 
-    // refactor to promises with sth like $.all().then()...
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(terms));
+var _sanitize = function(str) {
+    return str.replaceAll("\r\n", " ").replaceAll("\r", " ").replaceAll("\n", " ").trim().replaceAll("  ", " ").replaceAll("   ", " ").replace(" TOP", "").trimRight();
 };
 
 exports = module.exports = scrape1;
