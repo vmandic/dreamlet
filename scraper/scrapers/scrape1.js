46bc911f99e570c693f3c87a8c13f0b345842156
vmandic <mandic.vedran@gmail.com>
2016-10-29 21:23:00 +0200

    finished scraper 1, good 99% but not perfect

--- scraper/scrapers/scrape1.js
+++ scraper/scrapers/scrape1.js
@@ -14,12 +14,14 @@ var scrape1 = function (res) {
     "abcdefghijklmnopqrstuvwxyz".split("").forEach(function (letter, index) {
         requests.push(function (callback) {
             var url = baseUrl.format("", letter);
+            // remove "_all" from these letters as these sites do not have the aggregated list
+            url = url.replace(/([jkquvxyz])(_all).htm/, "$1.htm");
             limitedRequest(url, _processScrape1.bind(this, baseUrl, letter, callback));
         });
     });
 
     // initiate all requests
-    async.parallel(requests, function(error, results){
+    async.parallel(requests, function (error, results) {
         res.setHeader('Content-Type', 'application/json');
         res.json(results);
     });
@@ -46,38 +48,49 @@ var _saveToFileSystem = function (baseUrl, letter, html) {
 };
 
 var _saveToJson = function ($, callback) {
-    var terms = [], 
+    var terms = [],
         data = [],
         currentTerm = null;
 
     // filter out data for postprocessing
-    data = $("table[width=950] td[width=750]:first-child > p").map(function (index, el) {
-        if ($(el).html().indexOf("<b>") > -1 
-         || $(el).html().indexOf("<strong>") > -1)
-            return "TERM - " + _sanitize($(el).text());
+   data = $("table[width=950] td[width=750]:first-child > *").map(function (index, el) {
+        if ($(el).html().indexOf("<script") === -1) {
+            if ($(el).text().split("/")[0].split(" ").length < 4 && $(el).text().indexOf("*Please") === -1)
+                return "TERM - " + _sanitize($(el).text());
 
-        return _sanitize($(el).text());
+            return _sanitize($(el).text());
+        }
+
+        // Skip if contains a script tag
+        return "";
     });
 
     // build term objects
     data.toArray().forEach(function (e, i) {
-        if (e.indexOf("TERM - ") > -1) {
-            var termName = e.split("TERM - ")[1];
-            currentTerm = {
-                name: termName,
-                explanations: []
-            };
+        if (e.length !== 0) {
+            if (e.indexOf("TERM - ") > -1) {
+                var termName = e.split("TERM - ")[1];
 
-            terms.push(currentTerm);
-        } else {
-            currentTerm && e.length > 0 && currentTerm.explanations.push(e);
+                currentTerm = {
+                    name: termName,
+                    explanations: []
+                };
+
+                terms.push(currentTerm);
+            } else {
+                currentTerm && e.length > 0 && currentTerm.explanations.push(e);
+            }
         }
     });
-                                                        
-    callback(null, terms.filter(function(t) { return t.explanations.some(function(_t){ return _t.indexOf("function") == -1 }); }));
+
+    callback(null, terms.filter(function (t) {
+        return t.explanations.some(function (_t) {
+            return _t.indexOf("function") === -1;
+        });
+    }));
 };
 
-var _sanitize = function(str) {
+var _sanitize = function (str) {
     return str.replaceAll("\r\n", " ").replaceAll("\r", " ").replaceAll("\n", " ").trim().replaceAll("  ", " ").replaceAll("   ", " ").replace(" TOP", "").trimRight();
 };
 
