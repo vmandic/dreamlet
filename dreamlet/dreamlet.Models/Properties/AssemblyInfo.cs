bfd3a842856adb4de166d9cab80e263829ba784d
vmandic <mandic.vedran@gmail.com>
2016-11-20 10:19:31 +0100

    add empty classes, restructured the proj

--- dreamlet/dreamlet.Models/Properties/AssemblyInfo.cs
+++ dreamlet/dreamlet.Models/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dreamlet.WebAppDtoModels")]
+[assembly: AssemblyTitle("dreamlet.Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dreamlet.WebAppDtoModels")]
+[assembly: AssemblyProduct("dreamlet.Models")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- dreamlet/dreamlet.WebService/Properties/AssemblyInfo.cs
+++ dreamlet/dreamlet.WebService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dreamlet.WebAppService")]
+[assembly: AssemblyTitle("dreamlet.WebService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dreamlet.WebAppService")]
+[assembly: AssemblyProduct("dreamlet.WebService")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
