2522b79833c422910d19804fb76bc86aa058b8b8
vmandic <mandic.vedran@gmail.com>
2017-01-15 20:54:21 +0100

    added crypto tests

---
+++ dreamlet.server/dreamlet.Generic.Tests/Utilities/DreamletCryptoTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using dreamlet.Utilities;
+
+namespace dreamlet.Generic.Tests.Utilities
+{
+	[TestClass]
+	public class DreamletCryptoTests
+	{
+		static Random rnd = new Random();
+
+		[TestMethod]
+		public void Should_encrypt_and_decrypt_100_random_strings()
+		{
+			// ARRANGE & ACT
+			var pairs = Enumerable.Range(1, 100).Select(x => Tuple.Create(x, DreamletCrypto.Encrypt(x))).ToList();
+
+			// ASSERT
+			Assert.IsTrue(pairs.TrueForAll(x => x.Item1 == DreamletCrypto.DecryptToInt(x.Item2)));
+		}
+	}
+}
