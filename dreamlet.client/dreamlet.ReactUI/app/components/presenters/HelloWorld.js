1f65e00648c1e71400f72aa19ebc8a1c78698884
vmandic <mandic.vedran@gmail.com>
2017-01-17 00:14:32 +0100

    inited react cli app, enabled CORS on server

---
+++ dreamlet.client/dreamlet.ReactUI/app/components/presenters/HelloWorld.js
@@ -0,0 +1,11 @@
+﻿var React = require("react");
+
+var HelloWorld = React.createClass({
+	render: function () {
+		return (
+			<div>Hello from React!</div>
+		);
+	}
+});
+
+module.exports = HelloWorld;
--- dreamlet.server/dreamlet.WebService/Controllers/BaseController.cs
+++ dreamlet.server/dreamlet.WebService/Controllers/BaseController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace dreamlet.WebService.Controllers
 {
+	[EnableCors("*", "*", "*")]
     public class BaseController : ApiController
     {
 		public BaseController()
